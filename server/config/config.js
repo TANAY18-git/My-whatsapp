08d14c9d56a18a2fa9e63a6c88361da725e361a6
TANAY18 <tanay18@gmail.com>
2025-04-30 13:30:34 +0530

    Update color scheme to purple theme and change MongoDB connection string

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: 5000,
-  MONGODB_URI: 'mongodb+srv://gaming182006:tanay4328@cluster0.g7yen.mongodb.net/whatsapp_web',
+  MONGODB_URI: 'mongodb+srv://anmoldiscord4328:anmol4328@ak-chats.bhaiorc.mongodb.net/whatsapp_web',
   JWT_SECRET: 'whatsapp_web_secret_key',
   NODE_ENV: 'development'
 };
